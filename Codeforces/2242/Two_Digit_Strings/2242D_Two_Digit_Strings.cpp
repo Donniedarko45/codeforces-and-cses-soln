8c3f0d251b32d074d0a01bb4fab302188e108488
Kartikey Pandey <134577536+Donniedarko45@users.noreply.github.com>
2026-07-08 19:45:58 +0530

    Solved Codeforces 2242D

---
+++ Codeforces/2242/Two_Digit_Strings/2242D_Two_Digit_Strings.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void upd(int &x, int y) {
+    if (x < y) x = y;
+}
+
+void solve() {
+    string s, t;
+    cin >> s >> t;
+
+    int n = s.size();
+    int m = t.size();
+
+    vector<int> ps(n + 1), pt(m + 1);
+
+    for (int i = 0; i < n; i++)
+        ps[i + 1] = (ps[i] + (s[i] - '0')) % 10;
+
+    for (int i = 0; i < m; i++)
+        pt[i + 1] = (pt[i] + (t[i] - '0')) % 10;
+
+    n++;
+    m++;
+
+    if (ps.back() != pt.back()) {
+        cout << -1 << '\n';
+        return;
+    }
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+
+            if (i < n)
+                upd(dp[i + 1][j], dp[i][j]);
+            if (j < m)
+                upd(dp[i][j + 1], dp[i][j]);
+
+            if (i < n && j < m && ps[i] == pt[j])
+                upd(dp[i + 1][j + 1], dp[i][j] + 1);
+        }
+    }
+    cout << dp[n][m] - 1 << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--)
+        solve();
+}
